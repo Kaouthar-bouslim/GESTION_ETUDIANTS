3449d68c18c9d5ae574c8240b7e3ff6cb5655fec
Kaouthar-bouslim <78320053+Kaouthar-bouslim@users.noreply.github.com>
2021-12-28 05:49:09 +0100

    Update afficheretudiants.php

--- afficheretudiants.php
+++ afficheretudiants.php
@@ -1,7 +1,9 @@
 <?php
 $conn=mysqli_connect("localhost","root","") or die (mysqli_connect_error());
 mysqli_select_db($conn,"bd_scolarite") or die (mysqli_error($conn));
-$req="SELECT * from etudiants";
+
+
+$req="SELECT * from etudiants" ;
 $rs=mysqli_query($conn,$req) or die (mysqli_error($conn));
 ?>
 
@@ -9,6 +11,11 @@ $rs=mysqli_query($conn,$req) or die (mysqli_error($conn));
 <html lang="en">
 
 <body>
+
+<form action="chercheretudiant.php" method="post">
+        Mot Clé: <input type="text" name="motcle">
+        <input type="submit" value="Rechercher">
+    </form>
     <table border="1">
         <tr>
         <th>APOGEE</th><th>NOM</th><th>PHOTO</th>
@@ -19,6 +26,7 @@ $rs=mysqli_query($conn,$req) or die (mysqli_error($conn));
                 <td><?php echo $et['NOM'] ?> </td>
                 <td><img src="photo/<?php echo $et['PHOTO'] ?>" width=250px> </td>
                 <td><a href="supprimeretudiant.php?APOGEE=<?php echo $et['APOGEE'] ?> "> Supprimer </a></td>
+                <td><a href="editeretudiant.php?APOGEE=<?php echo $et['APOGEE'] ?> "> Editer </a></td>
             </tr>
         <?php } ?>
     </table>
